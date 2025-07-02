5fe2be92e611355bb2583498e6d3a165fcf733d4
Linus Mühlpointner <l.muehlpointner@web.de>
2025-07-02 17:58:47 +0200

    .

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,39 +1,27 @@
+"use Client"
 import { Suspense } from 'react';
 import List from "@/components/list"
 import Searchbar from "@/components/searchbar"
 import ListSkeleton from '@/components/skeleton/listSkeleton';
+import type { RecipePreview } from "@/types/recipe"
 
 // add real data fetch
 async function RecipesList() {
   // fake delay to test the loading state
   await new Promise(resolve => setTimeout(resolve, 2000));
   
-  //const recipes = await fetchRecipes(); 
-  //delete from here
-  const recipes = [
-    {
-      image: "https://images.unsplash.com/photo-1551024506-0bccd828d307?w=400",
-      title: "Classic Margherita Pizza",
-      description: "Italian • 30 mins",
-      id: "83u",
-    },
-    {
-      image: "https://images.unsplash.com/photo-1551024506-0bccd828d307?w=400",
-      title: "Chicken Carbonara", 
-      description: "Pasta • 25 mins",
-      id: "ws1",
-    }
-  ];
-//to here
+  const response = await fetch("@/api/recipes/list");
+  const recipes: RecipePreview[] = await response.json();
+
   return (
     <ul className="list bg-base-100 rounded-box shadow-md">
       {recipes.map((recipe) => (
         <List 
           key={recipe.id}
-          image={recipe.image} 
-          title={recipe.title} 
-          description={recipe.description} 
-          id={recipe.id}
+          image={recipe.image || ""} 
+          title={recipe.name} 
+          description={recipe.cuisine || ""} 
+          id={recipe.id.toString()}
         />
       ))}
     </ul>
