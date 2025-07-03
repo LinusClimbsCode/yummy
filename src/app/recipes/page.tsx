c4ac01c72fae7181cf43e8ae1ed612e7273cbab0
Linus Mühlpointner <l.muehlpointner@web.de>
2025-07-03 09:43:24 +0200

    commiting for rebase

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,32 +1,31 @@
-"use Client"
 import { Suspense } from 'react';
 import List from "@/components/list"
 import Searchbar from "@/components/searchbar"
 import ListSkeleton from '@/components/skeleton/listSkeleton';
 import type { RecipePreview } from "@/types/recipe"
+import { fetchRecipes } from "@/lib/fetchRecipes";
 
 // add real data fetch
 async function RecipesList() {
   // fake delay to test the loading state
   await new Promise(resolve => setTimeout(resolve, 2000));
-  
-  const response = await fetch("@/api/recipes/list");
-  const recipes: RecipePreview[] = await response.json();
 
-  return (
-    <ul className="list bg-base-100 rounded-box shadow-md">
-      {recipes.map((recipe) => (
-        <List 
-          key={recipe.id}
-          image={recipe.image || ""} 
-          title={recipe.name} 
-          description={recipe.cuisine || ""} 
-          id={recipe.id.toString()}
-        />
-      ))}
-    </ul>
-  );
-}
+  const recipes = await fetchRecipes();
+  
+    return (
+      <ul className="list bg-base-100 rounded-box shadow-md">
+        {recipes.map((recipe) => (
+          <List 
+            key={recipe.id}
+            image={recipe.image || ""} 
+            title={recipe.name} 
+            description={recipe.cuisine || ""} 
+            id={recipe.id.toString()}
+          />
+        ))}
+      </ul>
+    );
+  } 
 
 export default function Page() {
   return (
