2c2fa400f4d0b99b7b95d4f04f4417347e96c853
Linus Mühlpointner <l.muehlpointner@web.de>
2025-07-09 13:21:28 +0200

    import all Lucide Icons

--- src/components/recipeCardHeader.tsx
+++ src/components/recipeCardHeader.tsx
@@ -1,3 +1,7 @@
+// IMPORTS
+import { Heart, NotebookPen, Trash2, Link as LucideLink, Clock, ChefHat, ChartNoAxesColumnIncreasing as Chart, ShoppingCart, Printer } from 'lucide-react'; // Import Icons
+
+// TYPE
 type RecipesCardProps = {
     title?: string;
     image?: string;
@@ -31,9 +35,7 @@ export default function RecipeCardHeader({
                 {/* Action buttons overlay */}
                 <div className="absolute top-4 right-4 flex gap-2">
                     <button className="btn btn-circle btn-sm bg-base-100/80 hover:bg-base-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z" />
-                        </svg>
+                        <LucideLink />
                     </button>
                 </div>
             </figure>
